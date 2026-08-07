26cb86cdaaf0704123a0122fab99d1e83489d223
José Fernández <jfernandezconcha@gmail.com>
2026-08-07 19:15:23 -0400

    Refactoriza agenda y desacopla el formulario de reservas

--- assets/js/reservas.js
+++ assets/js/reservas.js
@@ -4,9 +4,24 @@
 // ABRIR FORMULARIO DE RESERVA
 //=====================================================
 
+/*document.querySelectorAll('.agenda-libre').forEach(boton => {
+
+    boton.addEventListener('click', function () {
+
+        window.location.href = this.dataset.url;
+
+    });
+
+});*/
+
+/*console.log("reservas.js cargado");*/
+
 document.querySelectorAll('.agenda-libre').forEach(boton => {
 
     boton.addEventListener('click', function () {
+
+       /* console.log("CLICK");
+        console.log(this.dataset.url);*/
 
         window.location.href = this.dataset.url;
 
